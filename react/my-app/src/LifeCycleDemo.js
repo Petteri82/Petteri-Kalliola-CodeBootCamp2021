e9e1292c8b12349c22b2c95af58d3d60082078b8
Petteri Kalliola <Petteri.kalliola@gmail.com>
2021-11-09 13:08:23 +0200

    first test

--- react/my-app/src/LifeCycleDemo.js
+++ react/my-app/src/LifeCycleDemo.js
@@ -2,15 +2,34 @@ import React from 'react';
 
 class LifeCycleDemo extends React.Component {
     constructor(props) {
-        super(props)
-    }
-    render() {
-        return <h1> Helllo this is lifecycle component</h1>;
-
+        super(props);
+        console.log("LifeCycleDemo.constructor");
     }
 
     componentDidMount() {
-        
+        console.log("In LifeCycleDemo.componentDidMount");
+        this.state = {title: ""};
+
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                this.setState({ title: json.title});
+                console.log("Component state has been modified")
+            });    
+            console.log("Fetch call has been made");
+    }
+    render() {
+        console.log("In lifecycleDemo.render");
+            return (
+            <div>
+                <h1> Helllo this is lifecycle component</h1>;
+                <p>Title: {this.state.title?}</p>
+            </div>);
+            }            
+
+    
+
     }
 }
 
